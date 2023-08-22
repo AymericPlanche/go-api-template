07f13d982e375a3ac8d78cbb3aa995a59c56a324
Frédéric Choquet <frederic.choquet@akeneo.com>
2023-08-22 18:25:54 +0200

    Remove unecessary config values

--- internal/app/config.go
+++ internal/app/config.go
@@ -6,34 +6,4 @@ type Config struct {
 
 	// If set to "local" some rules are loosen to make local development easier
 	Environment string
-
-	// Version tag
-	Version string
-
-	// GCP Project ID
-	ProjectID string
-
-	// When set, activates emulator mode for the firestore client
-	FirestoreEmulatorHost string
-
-	// CSRF secret
-	CSRFSecret string
-
-	// The root url of the auth callback. Must be configured accordingly in GCP API Credentials (Authorized redirect URIs)
-	AuthCallbackHost string
-
-	// Session passphrase
-	SessionPassphrase string
-
-	// Google client key
-	GoogleClientKey string
-
-	// Google secret
-	GoogleSecret string
-
-	// Target GCS Buckets
-	Buckets []string
-
-	// List of authorised user's email addresses
-	UserWhitelist []string
 }
--- main.go
+++ main.go
@@ -8,18 +8,8 @@ import (
 
 func main() {
 	app.Start(app.Config{
-		Port:                  env("PORT"),
-		Environment:           env("ENVIRONMENT"),
-		Version:               env("VERSION"),
-		ProjectID:             env("PROJECT_ID"),
-		FirestoreEmulatorHost: env("FIRESTORE_EMULATOR_HOST"),
-		CSRFSecret:            env("CSRF_SECRET"),
-		SessionPassphrase:     env("SESSION_PASSPHRASE"),
-		AuthCallbackHost:      env("AUTH_CALLBACK_HOST"),
-		GoogleClientKey:       env("GOOGLE_KEY"),
-		GoogleSecret:          env("GOOGLE_SECRET"),
-		Buckets:               strings.Split(env("GCS_BUCKETS"), "|"),
-		UserWhitelist:         strings.Split(env("USER_WHITE_LIST"), "|"),
+		Port:        env("PORT"),
+		Environment: env("ENVIRONMENT"),
 	})
 }
 
